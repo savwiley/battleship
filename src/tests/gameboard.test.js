e16526c833f128113e5027237ee37718fdc2eced
Savannaha <savwiley@yahoo.com>
2021-04-22 15:09:30 -0500

    created tests for X & Y positions

--- src/tests/gameboard.test.js
+++ src/tests/gameboard.test.js
@@ -1,13 +1,25 @@
 import gameboard from "../facts/gameboard.js";
 
-test("board test 1", () => {
+test("board test up/down", () => {
   expect(gameboard("Carrier", "place", "A", "1")).toEqual(
     {
       "name": "Carrier",
       "length": 5,
-      "coords": ["A1", "A2", "A3", "A4", "A5" ],
+      "coords": [ "A1", "A2", "A3", "A4", "A5" ],
       "targets": [ 1, 2, 3, 4, 5 ],
       "sunk": false
     }
   )
-})
+});
+
+test("board test left/right", () => {
+  expect(gameboard("Carrier", "place", "A", "1")).toEqual(
+    {
+      "name": "Carrier",
+      "length": 5,
+      "coords": [ "A1", "B1", "C1", "D1", "E1" ],
+      "targets": [ 1, 2, 3, 4, 5 ],
+      "sunk": false
+    }
+  )
+});
