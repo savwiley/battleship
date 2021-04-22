2afe883229105222527e61d58101c57f9f486cd0
Savannaha <savwiley@yahoo.com>
2021-04-22 14:11:16 -0500

    added placement logic

--- src/facts/gameboard.js
+++ src/facts/gameboard.js
@@ -1,24 +1,54 @@
 import Ships from "./ships.js";
 import shipList from "../data/shipList.json";
 
+//this file processes clicks, deciphers action, and sends commands accordingly
+
 const gameboard = (ship, action, x, y) => {
 
   //grab the ship
   const shipArr = shipList.filter((e) => e.name === ship);
+  //X array
+  const xArr = [ "A", "B", "C", "D", "E", "F", "G", "H", "I", "J" ];
 
   // shipArr[0][""]
 
   if (action === "place") {
-    //call logic that place ship on the grid
+    //make directional placement parameter
+    if (/*up/down*/) {
+      for (let i = y; i <= shipArr[0]["length"]; i++) {
+        const yCoord = x + i; //produces A1
+        shipArr[0]["coords"].push(yCoord); //saves A1 to array
+      }
+    } else if (/*left/right*/) {
+      const index = xArr.findIndex(e => e === x);
+      for (let i = index; i < shipArr[0]["length"]; i++) {
+        const xCoord = xArr[i] + y;
+        shipArr[0]["coords"].push(xCoord);
+      }
+    }
   } else if (action === "attack") {
-    //send it over to Ships
+    //add logic that deciphers where the mark is according to the coords
+
+    /*coord index could be used to find the target number element
+    > if shipArr[0]["coords"][1] then remove 1 from target array
+        "coords": [A2, A3],
+        "targets": [1, 2]
+          >>> HITS MARK A2
+        "coords": [A2, A3],
+        "targets": [2]
+    */
+
+    Ships(shipArr[0]["name"], /*MARK*/)
   } else if (action === "miss") {
     //??? make a new func that just handles these?
   }
-
-  //x = "3";
-  //y = "B";
+  
+  //x = "B";
+  //y = "3";
   //perhaps I should combine them in one parameter like "3B"
+  // >> combining them would mean making links for each block instead of tracking by row and column. but how do I do that??s
+  // x = A-E; y = 1-5
+  // x left/right (letters), y up/down (numbers)
 
 
   /*
@@ -28,14 +58,10 @@ const gameboard = (ship, action, x, y) => {
   */
 
 
-  return ({ ship, action, x, y })
-
-
+  return shipList[0];
 }
 
 export default gameboard;
-
-// x left/right (letters), y up/down (numbers)
 
 
 
