868ed97fb8ffda5cef5e1b8058fc1463d4d2e1d7
Savannaha <savwiley@yahoo.com>
2021-04-26 17:51:26 -0500

    added placement direction logic

--- src/App.js
+++ src/App.js
@@ -1,12 +1,49 @@
-import React from "react";
+import React, { useEffect } from "react";
+//import { Link } from "react-router-dom";
 import Grid from "./components/Grid.js";
 
+//this file will be for placing ships
+
 function App() {
+
+  //variables
+  let place = "placeH";
+  let ship;
+
+  //placement direction logic
+  useEffect(() => {
+    const dirBtn = document.querySelector(".directionBtn");
+
+    const direction = () => {
+      dirBtn.textContent = "Place Ships Horizontally";
+      place = "placeV";
+    }
+
+    if (dirBtn) {
+      dirBtn.addEventListener("click", direction);
+    }
+  })
+
+
+    /**
+     * Create logic to place ships
+     * + Create logic to turn ships vertical or horizontal
+     * - Create individual ships that player can select
+     * - Fill in the squares using Grid that the player selects
+     * - Send ship choice and hori/vert via params to Grid
+     * 
+     * 
+     * Action place can be "placeV" & "placeH" to decide vert/hori & I can just input || in the conditional.
+     */
 
   return (
     <div className="App">
       
-      <Grid />
+      <Grid place={ place } />
+
+      <button class="directionBtn">Place Ships Vertically</button>
+
+      
     
     </div>
   );
