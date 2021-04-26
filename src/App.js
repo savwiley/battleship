349ab4209e32fe484b7268a23d3b8d19fb70c5fa
Savannaha <savwiley@yahoo.com>
2021-04-26 12:11:41 -0500

    removed comments

--- src/App.js
+++ src/App.js
@@ -1,23 +1,7 @@
 import React from "react";
 import Grid from "./components/Grid.js";
-//import gameboard from "./facts/gameboard.js";
 
 function App() {
-
-  /*
-  const square = document.querySelectorAll(".square");
-  if (square) {
-    square.map(e => {
-      e.id.split(",");
-      if (e.class !== "missed") {
-        e.addEventListener("click", () => {
-          gameboard(`ship`, `action`, e.id[0], e.id[1])
-        })
-      }
-    }
-  )}
-  */
-
 
   return (
     <div className="App">
