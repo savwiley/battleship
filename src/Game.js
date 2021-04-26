9e1bbe2b9b2ff91b1f4e7246c98f983eb8e637d3
Savannaha <savwiley@yahoo.com>
2021-04-26 17:50:40 -0500

    created Game file

---
+++ src/Game.js
@@ -0,0 +1,23 @@
+import React from "react";
+//import gameboard from "./facts/gameboard.js";
+//import Grid from "./components/Grid.js";
+
+//this file will be for actually playing the game
+
+/**
+ * I think I should leave "Grid.js" for placement only. I can create a player board and AI board in separate component files.
+ */
+
+const Game = () => {
+
+  return (
+    <>
+    </>
+  )
+};
+
+export default Game;
+
+/**
+ * Make sure the coordinates that the player put in for each ship in "App.js" is on the player's board and the AI's ship placement is randomized without going over any previously taken squares.
+ */
