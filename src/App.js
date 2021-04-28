5bb01b4c5621e8338684db0b9650419c125e0060
Savannaha <savwiley@yahoo.com>
2021-04-28 12:03:24 -0500

    added ship selection logic

--- src/App.js
+++ src/App.js
@@ -1,49 +1,79 @@
-import React, { useEffect } from "react";
-//import { Link } from "react-router-dom";
+import React, { useRef, useEffect } from "react";
+import { Link } from "react-router-dom";
 import Grid from "./components/Grid.js";
 
 //this file will be for placing ships
 
 function App() {
-
-  //variables
-  let place = "placeH";
-  let ship;
+  const place = useRef("placeH");
+  const ship = useRef();
 
   //placement direction logic
   useEffect(() => {
     const dirBtn = document.querySelector(".directionBtn");
 
     const direction = () => {
-      dirBtn.textContent = "Place Ships Horizontally";
-      place = "placeV";
+      if (place.current === "placeH") {
+        dirBtn.textContent = "Place Ships Horizontally";
+        place.current = "placeV";
+      } else if (place.current === "placeV") {
+        dirBtn.textContent = "Place Ships Vertically";
+        place.current = "placeH";
+      }
     }
 
     if (dirBtn) {
       dirBtn.addEventListener("click", direction);
     }
-  })
+  });
+
+
+  //ship selection logic
+  useEffect(() => {
+    const shipContList = Array.from(document.querySelectorAll(".ship"));
+
+    const nameShip = (e) => {
+      ship.current = e.textContent;
+    }
+
+    if (shipContList) {
+      shipContList.map(e => {
+        return e.addEventListener("click", () => {
+          nameShip(e);
+        });
+      })
+    }
+  });
 
 
     /**
      * Create logic to place ships
-     * + Create logic to turn ships vertical or horizontal
-     * - Create individual ships that player can select
      * - Fill in the squares using Grid that the player selects
-     * - Send ship choice and hori/vert via params to Grid
+     * - Create route link that'll take you to the Game.js after all ships are on the grid
      * 
-     * 
-     * Action place can be "placeV" & "placeH" to decide vert/hori & I can just input || in the conditional.
+     * FA dots & circles for pegs/ships
      */
 
   return (
     <div className="App">
       
-      <Grid place={ place } />
+      <Grid place={place} ship={ship} />
 
-      <button class="directionBtn">Place Ships Vertically</button>
+      <button className="directionBtn">Place Ships Vertically</button>
 
-      
+      <div className="shipContainer">
+        <div className="ship">Carrier</div>
+        <div className="ship">Battleship</div>
+        <div className="ship">Destroyer</div>
+        <div className="ship">Submarine</div>
+        <div className="ship">Patrol Boat</div>
+      </div>
+
+      <Link to="/Game">
+        <div className="playBtn">
+          Play Game!
+        </div>
+      </Link>
     
     </div>
   );
