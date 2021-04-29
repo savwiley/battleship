54ecac1599844298f048004d8319f9ae70b62788
Savannaha <savwiley@yahoo.com>
2021-04-29 13:21:48 -0500

    added comments

--- src/facts/checkCoord.js
+++ src/facts/checkCoord.js
@@ -3,6 +3,18 @@ import enemyList from "../data/enemyShipList.json";
 
 //this file checks to see if any squares are taken by ships/misses/etc.
 
+//I still need this but I factored it all wrong. I've already got logic to dictate attacks/misses/etc and I already have logic to remove event listeners
+
+//I need this file to work almost solely for the AI to check coordinates for placing ships and attacking the players board.
+
+//I already have logic that checks for where the ships are and if the coord hit any of them on both/either board.
+
+//I need it to be able to (on its OWN board) know where available squares are to place its own ships and (on PLAYER's board) know where available squares are that weren't previously clicked.
+
+//Can I do that by checking squares that only have event listeners? I doubt it. I could ensure that they don't have the added classes toggled in? Which means I have to go back and add toggled classes to attacked/sunken ships. (classes are .attacked & .missed)
+
+//BRUH https://www.javascripttutorial.net/dom/css/check-if-an-element-contains-a-class/
+
 const checkCoord = (player, coord) => {
   let check = true;
   let plTakenCoords = [];
