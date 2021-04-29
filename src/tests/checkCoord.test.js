b1138c4d45c416a9fbc10fbf93897a59ac9b31e5
Savannaha <savwiley@yahoo.com>
2021-04-29 12:47:13 -0500

    tests for taken squares

---
+++ src/tests/checkCoord.test.js
@@ -0,0 +1,13 @@
+import checkCoord from "../facts/checkCoord.js";
+
+//checks for e.targets
+test("getting a ship", () => {
+  expect(checkCoord("Carrier")).toEqual(
+    [1, 2, 3, 4, 5, 1, 2, 3, 4, 1, 2, 3, 1, 2, 3, 1, 2]
+  )
+})
+
+
+test.only("check coord", () => {
+  expect(checkCoord("human", "Ai")).toBe(true)
+})
