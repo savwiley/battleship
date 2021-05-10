4913ebae1e57ee6b0c129a105e2600630d3299ef
Savannaha <savwiley@yahoo.com>
2021-05-10 16:59:37 -0500

    moved logic to PlayerGrid.js

--- src/Game.js
+++ src/Game.js
@@ -1,37 +1,13 @@
-import React, { useEffect } from "react";
+import React from "react";
 //import gameboard from "./facts/gameboard.js";
-import shipList from "./data/shipList.json";
+//import shipList from "./data/shipList.json";
 import PlayerGrid from "./components/PlayerGrid.js";
 import AIGrid from "./components/AIGrid.js";
 
 //this file will be for actually playing the game
 
-
-//all logic below is for placing PLAYER ships on PLAYER grid
-
 const Game = () => {
-  const takenCoords = [];
 
-  shipList.map(e => {
-    for (let i = 0; i < e.length; i++) {
-      takenCoords.push(e.coords[i]);
-    }
-    return takenCoords;
-  });
-
-  useEffect(() => {
-    for (let i = 0; i < takenCoords.length; i++) {
-      const square = document.querySelector(`#${takenCoords[i]}`);
-      if (square) {
-        console.log("square ran");
-        console.log(document.querySelector(`#${takenCoords[i]}`));
-        square.classList.toggle("placed");
-      } else {
-        console.log(takenCoords);
-        console.log(document.querySelector(`#${takenCoords[i]}`))
-      }
-    }
-  });
 
   return (
     <>
