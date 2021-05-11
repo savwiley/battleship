cb2a241473a7454853d12f80c92959360e5082e5
Savannaha <savwiley@yahoo.com>
2021-05-11 14:35:12 -0500

    removed AI placing logic

--- src/Game.js
+++ src/Game.js
@@ -1,12 +1,53 @@
 import React from "react";
-//import gameboard from "./facts/gameboard.js";
-//import shipList from "./data/shipList.json";
+import gameboard from "./facts/gameboard.js";
+import enShipList from "./data/enemyShipList.json";
 import PlayerGrid from "./components/PlayerGrid.js";
 import AIGrid from "./components/AIGrid.js";
 
 //this file will be for actually playing the game
 
 const Game = () => {
+  const alpha = [ "A", "B", "C", "D", "E", "F", "G", "H", "I", "J" ];
+
+  //gets a random coord
+  const getCoord = () => {
+    const numbX = Math.floor(Math.random() * 10);
+    const letterX = alpha[numbX];
+    const numbY = Math.floor(Math.random() * 10);
+    console.log(letterX + numbY);
+    return letterX + numbY;
+  };
+
+  //gets a random direction
+  const getDirection = () => {
+    const numb = Math.floor(Math.random() * 2);
+    if (numb === 0) {
+      return "placeV";
+    } else if (numb === 1) {
+      return "placeH";
+    }
+  };
+
+  //gets a random, unplaced ship array
+  const getShip = () => {
+    for (let i = 0; i < enShipList.length; i++) {
+      if (enShipList[i]["coords"].length !== enShipList[i]["length"]) {
+        const shipArr = enShipList[i];
+        i = enShipList.length;
+        return shipArr["name"];
+      }
+    }
+  };
+
+  //AI places ships
+  for (let i = 0; i < 5; i++) {
+    gameboard("computer", getShip(), getDirection(), getCoord());
+    console.log(`I ran ${i} times!`);
+  };
+
+  /**
+   * figure out how to run it again if ship fails to place
+   */
 
 
   return (
