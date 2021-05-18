060ba0a9d14e368ff4a87539655fa45655ffc06a
Savannaha <savwiley@yahoo.com>
2021-05-18 16:40:55 -0500

    added header component

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="header">
+      Battleship
+    </div>
+  )
+}
+
+export default Header;
+
+/**
+ * add github link
+ */
