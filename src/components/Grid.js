a9a857cec0d7bbc592dd179c4b665d3d75038df3
Savannaha <savwiley@yahoo.com>
2021-05-19 15:07:45 -0500

    added play btn visibility only after all ships are placed

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -1,20 +1,48 @@
 import React, { useEffect } from "react";
 import gameboard from "../facts/gameboard.js";
+import shipList from "../data/shipList.json";
 
 const Grid = (props) => {
   const { place, ship } = props;
 
   const alpha = [ "A", "B", "C", "D", "E", "F", "G", "H", "I", "J" ];
+  let numb = 0;
 
   useEffect(() => {
     const square = Array.from(document.querySelectorAll(".square"));
     const grid = document.querySelector(".grid");
+    const shipMenuList = Array.from(document.querySelectorAll(".ship"));
+    const playBtn = document.querySelector(".playBtn");
+    playBtn.style.display = "none";
 
     const changeSquare = (e) => {
       if (!ship.current) {
         alert("Please choose a ship!")
       } else {
         gameboard("human", ship.current, place.current, e.id);
+        //only dims ship button after ship is placed
+        //only shows play button after ALL ships are placed
+        //there has got to be a better way to do this
+        shipMenuList.map(e => {
+          if (e.textContent === ship.current) {
+            shipList.map(a => {
+              if (e.textContent === a["name"]) {
+                if (a["coords"].length === a["length"]) {
+                  e.style.background = "#929292";
+                  e.style.color = "#616161";
+                  e.style.outline = "1px solid #fff";
+                  numb++;
+                };
+              };
+              return a;
+            });
+          };
+          return e;
+        });
+        if (numb === 5) {
+          playBtn.style.display = "block";
+        };
+        ship.current = null;
       }
     };
 
